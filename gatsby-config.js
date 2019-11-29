0992d3cc565a70b6b16e32301aa841e119c783b7
Fergus Farrell <fergusf@orionhealth.com>
2019-11-30 12:07:50 +1300

    config changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,9 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: process.env.DEPLOY_URL
+          ? `https://intense-forest-18182.herokuapp.com`
+          : `http://localhost:1337`,
         queryLimit: 100,
         contentTypes: [`blog`, `project`],
       },
