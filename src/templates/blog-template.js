561ef689cf35dab2acf6e292a11444977d365b35
Fergus Farrell <fergusf@orionhealth.com>
2019-12-01 15:00:18 +1300

    added SEO to templates

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -2,6 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import ReactMarkdown from "react-markdown"
 
+import SEO from "../components/seo"
 import CodeBlock from "../components/code-block"
 import PostNavigation from "../components/post-navigation"
 
@@ -11,22 +12,27 @@ const BlogTemplate = ({
   },
 }) => {
   return (
-    <div className="blog-view">
-      <h1>{title}</h1>
-      <h5 className="highlight">{`${authored_date} • ${time_to_read} minute read`}</h5>
-      <hr />
-      <div className="blog-content">
-        <ReactMarkdown
-          source={content}
-          renderers={{ code: CodeBlock }}
-          transformImageUri={uri =>
-            uri.startsWith("http") ? uri : `${process.env.IMAGE_BASE_URL}${uri}`
-          }
-        />
+    <>
+      <SEO title={title} />
+      <div className="blog-view">
+        <h1>{title}</h1>
+        <h5 className="highlight">{`${authored_date} • ${time_to_read} minute read`}</h5>
+        <hr />
+        <div className="blog-content">
+          <ReactMarkdown
+            source={content}
+            renderers={{ code: CodeBlock }}
+            transformImageUri={uri =>
+              uri.startsWith("http")
+                ? uri
+                : `${process.env.IMAGE_BASE_URL}${uri}`
+            }
+          />
+        </div>
+        <hr />
+        <PostNavigation next={next} prev={prev} postType="blog" />
       </div>
-      <hr />
-      <PostNavigation next={next} prev={prev} postType="blog" />
-    </div>
+    </>
   )
 }
 
